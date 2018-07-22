5bb601af041422256a36b71a44b26ad7982cdd61
KKonkov <kskonkov@yandex.ru>
2018-07-22 17:34:26 +0400

    1.3. JavDoc. Code convetion.

---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,18 @@
+package ru.job4j.calculate;
+/**
+ * Calculate.
+ * @author Kirill Konkov (kskonkov@mail.ru)
+ * @since 18.07.2018
+ * @version 1
+*/
+public class Calculate {
+/**
+* main.
+* Конструктор, вывод строки в консоль.
+* @param args - args.
+* @return Hello World
+*/	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
